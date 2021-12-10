43e1c457d7d5ebf3393ca2b61bbed8ed0479f804
Schrodingar <iv.aboyan@gmail.com>
2021-12-10 14:35:36 +0200

    Update project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        Searcher searcher = new Searcher();
-        String i = searcher.countObjectsWithSpecificFieldOnly("C:\\Users\\ivabo\\IdeaProjects\\globallogicTask\\src\\main\\java\\JSON", "address1");
-//        String i = searcher.countObjectsWithSpecificFieldAndSpecificValue("C:\\Users\\ivabo\\IdeaProjects\\globallogicTask\\src\\main\\java\\JSON", "weight", "229");
+
     }
 }
--- src/test/java/SearcherTest.java
+++ src/test/java/SearcherTest.java
@@ -14,13 +14,30 @@ class SearcherTest {
 
     @ParameterizedTest
     @MethodSource("countObjectsWithSpecificFieldOnlyDataProvider")
-    public void method1(String expected, String param1, String param2) {
+    public void testCountObjectsWithSpecificFieldOnly(String expected, String param1, String param2) {
         assertEquals(expected, searcher.countObjectsWithSpecificFieldOnly(param1, param2));
     }
 
     public static Stream<Arguments> countObjectsWithSpecificFieldOnlyDataProvider() {
         return Stream.of(
-                arguments("found 3 objects with field \"address1\"", "tests\\searcherTests\\JSON", "address1")
+                arguments("found 3 objects with field \"address1\"", "src/test/java/JSON", "address1"),
+                arguments("found 0 objects with field \"idCode\"", "src/test/java/JSON", "idCode"),
+                arguments("found 1 objects with field \"refCode\"", "src/test/java/JSON", "refCode")
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("countObjectsWithSpecificFieldAndSpecificValueDataProvider")
+    public void testCountObjectsWithSpecificFieldAndSpecificValue(String expected, String param1, String param2, String param3) {
+        assertEquals(expected, searcher.countObjectsWithSpecificFieldAndSpecificValue(param1, param2, param3));
+    }
+
+    public static Stream<Arguments> countObjectsWithSpecificFieldAndSpecificValueDataProvider() {
+        return Stream.of(
+                arguments("found 3 objects where field \"weight\" equals \"229\"", "src/test/java/JSON", "weight", "229"),
+                arguments("found 2 objects where field \"desc\" equals \"Driver Count Required\"", "src/test/java/JSON", "desc", "Driver Count Required"),
+                arguments("found 5 objects where field \"contactFunctionCode\" equals \"CN\"", "src/test/java/JSON", "contactFunctionCode", "CN")
+        );
+    }
+
 }
